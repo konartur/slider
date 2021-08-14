a4a195402085dee180516e63242065a1351810e2
Vadimator <vpetrasenko@gmail.com>
2021-08-14 14:59:36 +0300

    added basic handler to navigation

--- src/slider.js
+++ src/slider.js
@@ -1,7 +1,12 @@
 import './slider.scss';
 
 export default class Slider {
-  constructor(selector) {
+  get wrapperWidth() {
+    return this.wrapper.clientWidth;
+  }
+
+  constructor(selector, options = {}) {
+    this.activeIndex = 0;
     this.container = document.querySelector(selector);
 
     if (!this.container) {
@@ -9,20 +14,40 @@ export default class Slider {
     }
 
     this.wrapper = this.container.querySelector('.slider-wrapper');
-    const wrapperWidth = this.wrapper.clientWidth;
 
     this.sliders = this.wrapper.querySelectorAll('.slider-slide');
 
     // eslint-disable-next-line no-restricted-syntax
     for (const slider of this.sliders) {
-      slider.style.width = `${wrapperWidth}px`;
+      slider.style.width = `${this.wrapperWidth}px`;
     }
 
-    let currentSlide = 0;
+    if ('navigation' in options) {
+      if ('nextEl' in options.navigation) {
+        const nextEl = this.container.querySelector(options.navigation.nextEl);
+        if (nextEl) {
+          nextEl.addEventListener('click', this.slideNext.bind(this));
+        }
+      }
 
-    this.wrapper.addEventListener('click', () => {
-      currentSlide += 1;
-      this.wrapper.style.transform = `translate3d(-${wrapperWidth * currentSlide}px, 0px, 0px)`;
-    });
+      if ('prevEl' in options.navigation) {
+        const prevEl = this.container.querySelector(options.navigation.prevEl);
+        if (prevEl) {
+          prevEl.addEventListener('click', this.slidePrev.bind(this));
+        }
+      }
+    }
+  }
+
+  slideNext() {
+    const nextIndex = this.activeIndex + 1;
+    this.wrapper.style.transform = `translate3d(-${this.wrapperWidth * nextIndex}px, 0px, 0px)`;
+    this.activeIndex = nextIndex;
+  }
+
+  slidePrev() {
+    const prevIndex = this.activeIndex - 1;
+    this.wrapper.style.transform = `translate3d(-${this.wrapperWidth * prevIndex}px, 0px, 0px)`;
+    this.activeIndex = prevIndex;
   }
 }

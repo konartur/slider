02e509f23930f3ec6bb30128539d2986c4fa293f
Vadimator <vpetrasenko@gmail.com>
2021-08-15 18:14:05 +0300

    remove code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import Slider from './slider';
-
 document.addEventListener('DOMContentLoaded', () => {
-  // eslint-disable-next-line no-new
-  new Slider('.slider-container', {
-    navigation: {
-      nextEl: '.slider-button-next',
-      prevEl: '.slider-button-prev'
-    }
-  });
+
 });
